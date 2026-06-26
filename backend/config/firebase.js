e6b6bf614506b143c97229a0fb7cedd77b03d263
Cryptic798 <dinghengng@gmail.com>
2026-06-26 13:21:56 +0800

    .

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -2,7 +2,10 @@ const admin = require('firebase-admin');
 
 let app;
 if (!admin.apps || admin.apps.length === 0) {
-  const serviceAccount = require('./serviceAccount.json');
+  const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_JSON
+    ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON)
+    : require('./serviceAccount.json');
+
   app = admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
   });
